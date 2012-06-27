b5ffd555d7579bc7fbd66823b054fd7d2564c2b4
Mike McClurg <mike.mcclurg@gmail.com>
2012-06-27 14:40:27 +0100

    Initial commit of empty test class.

---
+++ src/test/com/develogical/shopping/OrderDispatcherTest.java
@@ -0,0 +1,9 @@
+package com.develogical.shopping;
+
+import org.jmock.integration.junit4.JMock;
+import org.junit.runner.RunWith;
+
+@RunWith(value=JMock.class)
+public class OrderDispatcherTest {
+
+}
