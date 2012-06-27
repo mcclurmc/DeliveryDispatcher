9d8cc52f28f7ce14235f206008bbaec63e5e6de0
Mike McClurg <mike.mcclurg@gmail.com>
2012-06-27 14:52:51 +0100

    Added first test.

--- src/test/com/develogical/shopping/OrderDispatcherTest.java
+++ src/test/com/develogical/shopping/OrderDispatcherTest.java
@@ -1,9 +1,32 @@
 package com.develogical.shopping;
 
+import org.jmock.Expectations;
+import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(value=JMock.class)
 public class OrderDispatcherTest {
+    Mockery context = new Mockery();
+    private final Warehouse warehouse = context.mock(Warehouse.class);
 
+    private final OrderDispatcher orderDispatcher = new OrderDispatcher(warehouse);
+
+    @Test
+    public void itemIsInStock_ThenConfirmOrder() {
+        // Objects we need
+        final Order order = new Order("My Order");
+        final OrderTracker orderTracker = context.mock(OrderTracker.class);
+        Recipient recipient = new Recipient("","","");
+
+        context.checking(new Expectations() {{
+            oneOf(warehouse).hasStockOf(order);
+            will(returnValue(true));
+            oneOf(orderTracker).orderConfirmed(order);
+            ignoring(warehouse);
+        }});
+
+        orderDispatcher.placeOrder(recipient, order, orderTracker);
+    }
 }
